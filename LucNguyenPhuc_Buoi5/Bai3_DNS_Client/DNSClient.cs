b88e6e35c9be5bcdd114393110f6801be986fa48
parker1220 <nguyenphucluc1220@gmail.com>
2017-11-05 15:55:31 +0700

    DNS

---
+++ LucNguyenPhuc_Buoi5/Bai3_DNS_Client/DNSClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai3_DNS_Client
+{
+    internal class DNSClient
+    {
+        internal void requestResolve(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LucNguyenPhuc_Buoi5/Bai3_DNS_Server/DNSServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai3_DNS_Server
+{
+    internal class DNSServer
+    {
+        internal void startListening()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
